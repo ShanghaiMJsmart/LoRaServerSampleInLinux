ed5c5878e1d5d4238dd21bc533d7b32eab189d6c
Horse Ma <wei_technology@163.com>
2018-03-15 19:41:20 +0800

    delete Confirm FrameType

--- LoRaServerSample.c
+++ LoRaServerSample.c
@@ -17,9 +17,32 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <json-c/json.h>
+#include <time.h>
 
 #define PORT  32500
 #define QUEUE_SIZE   10
+#define SOCKET_RCV_BUFFER_SIZE	1024 * 5
+#define MAX_NODE_NUM	1024
+
+typedef struct{
+    uint8_t *strDevEUI[8 * 2 + 1];
+    uint32_t iDevAddr;
+    bool isClassC;    
+}st_ServerNodeDatabase,*pst_ServerNodeDatabase;
+
+void Hex2Str( const char *sSrc,  char *sDest, int nSrcLen )
+{
+    int  i;
+    char szTmp[3];
+
+    for( i = 0; i < nSrcLen; i++ )
+    {
+        sprintf( szTmp, "%02X", (unsigned char) sSrc[i] );
+        memcpy( &sDest[i * 2], szTmp, 2 );
+    }
+    //sDest[i * 2] = '\0';
+    return ;
+}
 
 void *server_send(void *arg)
 {
@@ -33,29 +56,127 @@ void *server_send(void *arg)
 
 void str_echo(int sockfd)
 {
-    uint8_t buffer[1024 * 100];
+    uint8_t buffer[SOCKET_RCV_BUFFER_SIZE];
     int len;
     int err;
-    //uint8_t *tmp;
-    //int index;
     pthread_t main_tid;
     struct json_object *pragma = NULL;
     struct json_object *obj = NULL;
-    uint8_t stringformat[256 * 2];
-    uint8_t tx_data_buf[300];
     uint8_t *pstrchr;
-    err = pthread_create(&main_tid, NULL, server_send, &sockfd); //创建线程
+    uint32_t iDevAddr = 0;
+    bool isconfirmrequest = false;
+    st_ServerNodeDatabase stServerNodeDatabase[MAX_NODE_NUM];
+    const uint8_t *sendData = "hello to node";
+    uint8_t datatosend[strlen(sendData) * 2 + 1];
+
+    Hex2Str( sendData, datatosend, strlen(sendData) );
+    datatosend[strlen(sendData) * 2] = '\0';
+    memset(stServerNodeDatabase,0,sizeof(st_ServerNodeDatabase));
+    srand((int)time(0));
+
     pid_t pid = getpid();
     while(1)
     {
         memset(buffer,0,sizeof(buffer));
         len = recv(sockfd, buffer, sizeof(buffer) - 1,0);
-        if(len < 0)
+        if(len < 1)
         {
             printf("child process: %d exited.\n",pid);
             break;
         }
         printf("pid:%d %dreceive: %s\r\n",pid,len,buffer);
+        printf("\r\n");
+	pstrchr = buffer;
+        while((pstrchr - buffer) < len)
+        {
+            pstrchr = strchr(pstrchr,'{');
+            if(pstrchr == NULL)
+            {
+                break;
+            }
+            else
+            {
+                pstrchr ++;
+            }
+            pragma = json_tokener_parse((const char *)(pstrchr - 1));
+            if(pragma == NULL)
+            {
+                break;
+            }
+
+            json_object_object_get_ex(pragma,"FrameType",&obj);
+            if(obj == NULL)
+            {
+		printf("Format eerror %d\r\n",__LINE__);
+                json_object_put(pragma);
+                break;
+            }
+            if(strcmp((const char*)json_object_get_string(obj),"UpData") == 0)
+            {
+		json_object_object_get_ex(pragma,"NetAddr",&obj);
+            	if(obj == NULL)
+            	{
+                    printf("Format eerror %d\r\n",__LINE__);
+                    json_object_put(pragma);
+                    break;
+            	}
+                iDevAddr = json_object_get_int(obj);
+
+                json_object_object_get_ex(pragma,"DevEUI",&obj);
+                if(obj == NULL)
+                {
+                    printf("Format eerror %d\r\n",__LINE__);
+                    json_object_put(pragma);
+                    break;
+                }
+                strcpy((char *)stServerNodeDatabase[iDevAddr].strDevEUI,(const char*)json_object_get_string(obj));
+
+                json_object_object_get_ex(pragma,"NodeType",&obj);
+                if(obj == NULL)
+                {
+                    printf("Format eerror %d\r\n",__LINE__);
+                    json_object_put(pragma);
+                    break;
+                }
+                if(strcmp("Class C",json_object_get_string(obj)))
+                {
+                    stServerNodeDatabase[iDevAddr].isClassC = true;
+                }
+                else
+                {
+                    stServerNodeDatabase[iDevAddr].isClassC = false;
+                }
+                json_object_object_get_ex(pragma,"ConfirmRequest",&obj);
+	        if(obj == NULL)
+                {
+                    printf("Format eerror %d\r\n",__LINE__);
+                    json_object_put(pragma);
+                    break;
+                }
+                isconfirmrequest = json_object_get_boolean(obj);
+                json_object_put(pragma);
+                pragma = json_object_new_object();
+                json_object_object_add(pragma,"FrameType",json_object_new_string("DownData"));
+                json_object_object_add(pragma,"NetAddr",json_object_new_int(iDevAddr));
+                json_object_object_add(pragma,"Port",json_object_new_int((223.0*rand()/(RAND_MAX+1.0))));
+                json_object_object_add(pragma,"ConfirmRequest",json_object_new_boolean((1.999*rand()/(RAND_MAX+1.0))));
+				json_object_object_add(pragma,"Confirm",json_object_new_boolean(isconfirmrequest));
+                json_object_object_add(pragma,"Data",json_object_new_string(datatosend));
+                len = strlen(json_object_to_json_string(pragma));
+                len = send(sockfd,json_object_to_json_string(pragma), len, 0);
+                if(len < 1)
+                {
+                    printf("reconnect %d\r\n",__LINE__);
+                    json_object_put(pragma);
+                    pthread_exit(NULL);
+                }
+                printf("%s",json_object_to_json_string(pragma));
+                printf("\r\n");
+                json_object_put(pragma);
+
+            }
+            //memset(buffer,0,1024 * 100);
+        }
     }
     close(sockfd);
 }
